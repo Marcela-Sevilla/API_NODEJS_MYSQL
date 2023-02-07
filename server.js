18e9aa4c791bbcad13ea89b9a8a2ae212a5e80f6
Marcela_Sevilla <sevillamarcela514@gmail.com>
2023-02-07 10:17:18 -0500

    Creando la conexión a la base de datos y las opciones

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express')
+const mysql = require('mysql')
+const myconn = require('express-myconnection')
+const dbOptions = {
+    host: 'localhost',
+    port: 3306,
+    use: 'root',
+    password: '',
+    database: 'library'
+}
 
 const app = express()
 app.set('port', process.env.PORT || 9000)
+
+// MIDDELWARES---------------------------------------------------------
+app.use(myconn(mysql, dbOptions, 'single'))
 
 // ROUTES---------------------------------------------------------
 app.get('/', (req, res)=>{
