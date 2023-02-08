5032bc83814d735a1decaa06c6d22a8844228c1e
Marcela Sevilla <sevillamarcela514@gamil.com>
2023-02-08 15:51:06 -0500

    Creando consulta a la BD y creando archivo de cliente REST

--- routes.js
+++ routes.js
@@ -2,7 +2,16 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', (req, res)=>{
-    res.send('Testing API')
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+
+        conn.query('SELECT * FROM books', (err, rows)=>{
+
+            if(err) return res.send(err)
+
+            res.json(rows)
+        })
+    })
 })
 
 module.exports = routes
